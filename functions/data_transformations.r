a517c1f854c790e2cb588ff71b7f6dbf34d3f141
Fabian Oschwald <fatechde@gmail.com>
2023-10-17 08:11:33 +0200

    forgot to save file, new attempt

--- functions/data_transformations.r
+++ functions/data_transformations.r
@@ -7,16 +7,16 @@ library(anytime)
 
 transform_metadata_to_df <- function(metadata) {
   metadata[[1]] %>% 
-    map(as_tibble) 
-    # list_rbind() %>% 
-    # mutate(latestData = map_chr(latestData, 1, .default = NA_character_)) %>% 
-    # mutate(latestData = as_datetime(latestData, tz = "UTC")) %>% 
-    # mutate(location = map(location, unlist)) %>% 
-    # mutate(
-    #   lat = map_dbl(location, "latLon.lat"),
-    #   lon = map_dbl(location, "latLon.lon")
-    # ) %>% 
-    # select(-location)
+    map(as_tibble) %>% 
+    list_rbind() %>%
+    mutate(latestData = map_chr(latestData, 1, .default = NA_character_)) %>%
+    mutate(latestData = as_datetime(latestData, tz = "UTC")) %>%
+    mutate(location = map(location, unlist)) %>%
+    mutate(
+      lat = map_dbl(location, "latLon.lat"),
+      lon = map_dbl(location, "latLon.lon")
+    ) %>%
+    select(-location)
 }
 
 # Problem 4a
@@ -32,9 +32,14 @@ actual_output <- to_iso8601(as_datetime("2016-09-01 10:11:12"),-4)
 wanted_output == actual_output
 
 transform_volumes <- function(volume_json) {
-  volume_json[[1]][[1]][[1]][[1]] %>% 
-    map(as_tibble) %>% 
-    list_rbind()
+  volume_json$trafficData$volume$byHour %>% 
+    as_tibble() %>% 
+    unnest_wider(edges) %>% 
+    unnest_wider(node) %>% 
+    unnest_wider(total) %>% 
+    unnest_wider(volumeNumbers) %>% 
+    mutate(from = as_datetime(from),
+           to = as_datetime(to))
 }
 
 
